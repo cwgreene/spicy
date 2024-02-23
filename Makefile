c0a60dc6f9cea58d9223067fa818aeacb6641543
Christopher Greene <archgoon@gmail.com>
2024-02-22 19:29:31 -0800

    Bare minimum initialization.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-cppspice:
+cppspice: test_spice.cpp
 	g++ -I ${HOME}/.local/include -L ./lib -Wl,-rpath '$$ORIGIN/lib' test_spice.cpp -lngspice -o cppspice
 ngspice:
 	ngspice -b test.spice
--- test_spice.cpp
+++ test_spice.cpp
@@ -1,13 +1,21 @@
 #include <ngspice/sharedspice.h>
 #include <stdio.h>
 
+int sendchar(char *c, int whichspice, void* data) {
+    printf("%s\t%d\t%s\n", c, whichspice, (char *) data);
+    return 0;
+}
+
 int main() {
+    const char *hello = "hello";
     int result = ngSpice_Init(
-        NULL, /* SendChar */
-        NULL, /* SendStat */
+        sendchar, /* SendChar: int (SendChar)(char *, int, void*)*/
+        NULL, /* SendStat: int (SendStat)(char *, int *, void *)*/
         NULL, /* ControlledExit */
-        NULL, /* SendData */
-        NULL, /* SendInitData */
+        NULL, /* SendData: int (SendData)(pvecvaluesall, int, int, void *)*/
+        NULL, /* SendInitData: int (SendInitData)(pvecinfoall, int, void *)*/
         NULL, /* BGThreadRunning */
-        NULL /* void * */);
+        (void *)hello);
+    printf("All done with init\n");
+    return 0;    
 }
